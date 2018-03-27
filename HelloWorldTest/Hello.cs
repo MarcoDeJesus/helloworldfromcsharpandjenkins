c70c15f3c4fcd7af2b0636576837cbf1219e9454
HERBALIFECORP\marcoantoniod <marcoantoniod@herbalife.com>
2018-03-27 09:31:05 -0600

    first commit

---
+++ HelloWorldTest/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldTest
+{
+    class Hello
+    {
+        static void main (string args[])
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World!";
+        }
+    }
+}
